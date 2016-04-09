d329e3ad5b179f455a2f452dcaf1ffa431616858
hztanxuewei <hztanxuewei@corp.netease.com>
2016-04-09 22:34:18 +0800

    demo

--- gallery/js/gallery.js
+++ gallery/js/gallery.js
@@ -12,9 +12,12 @@
         function showPic(whichPic){
 
             var placeholder = document.getElementById('placeholder');
+            var descp = document.getElementById('description');
             var source = whichPic.getAttribute('href');
+            var title = whichPic.getAttribute('title');
 
             placeholder.setAttribute('src', source);
+            descp.innerText = title;
         }
     }
 })();
