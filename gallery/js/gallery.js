ffa517066e393bb378d0693e757fa888100ddc79
tanxuewei <tanxuewei94@126.com>
2016-04-11 20:19:43 +0800

    更改文件

--- gallery/js/gallery.js
+++ gallery/js/gallery.js
@@ -1,23 +1,21 @@
-(function(){
-    window.onload = function(){
-        var ul = document.getElementsByTagName('ul')[0];
-        var li = document.getElementsByTagName('a');
+window.onload = function(){
+    var ul = document.getElementsByTagName('ul')[0];
+    var li = document.getElementsByTagName('a');
 
-        for (var i=0; i<li.length; i++){
-            li[i].onclick = function(){
-                showPic(this);
-                return false;
-            }
-        }
-        function showPic(whichPic){
-
-            var placeholder = document.getElementById('placeholder');
-            var descp = document.getElementById('description');
-            var source = whichPic.getAttribute('href');
-            var title = whichPic.getAttribute('title');
-
-            placeholder.setAttribute('src', source);
-            descp.innerText = title;
+    for (var i=0; i<li.length; i++){
+        li[i].onclick = function(){
+            showPic(this);
+            return false;
         }
     }
-})();
+    function showPic(whichPic){
+
+        var placeholder = document.getElementById('placeholder');
+        var descp = document.getElementById('description');
+        var source = whichPic.getAttribute('href');
+        var title = whichPic.getAttribute('title');
+
+        placeholder.setAttribute('src', source);
+        descp.innerText = title;
+    }
+}
