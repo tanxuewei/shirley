0776b0cbb3955ee00f4d74b6aed004e22adb74c6
tanxuewei <tanxuewei94@126.com>
2016-04-13 17:27:23 +0800

    6.3.1 gallery版本

--- gallery/js/gallery.js
+++ gallery/js/gallery.js
@@ -1,13 +1,6 @@
 window.onload = function(){
-    var ul = document.getElementsByTagName('ul')[0];
-    var li = document.getElementsByTagName('a');
-
-    for (var i=0; i<li.length; i++){
-        li[i].onclick = function(){
-            showPic(this);
-            return false;
-        }
-    }
+    prepareGallery();
+    
     function showPic(whichPic){
 
         var placeholder = document.getElementById('placeholder');
@@ -17,5 +10,25 @@ window.onload = function(){
 
         placeholder.setAttribute('src', source);
         descp.innerText = title;
+    }
+    function prepareGallery(){
+        if (!document.getElementsByTagName){
+            return;
+        }
+        if (!document.getElementById){
+            return;
+        }
+        if (!document.getElementById('imagegallery')){
+            return;
+        }
+        var gallery = document.getElementById('imagegallery');
+        var links = gallery.getElementsByTagName('a');
+
+        for (var i=0; i<links.length; i++){
+            links[i].onclick = function(){
+                showPic(this);
+                return false;
+            }
+        }
     }
 }
