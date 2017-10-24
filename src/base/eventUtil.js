cdef92b6e2f6caebe9eb383e751bd839e17e44ce
tanxuewei <tanxuewei94@126.com>
2017-10-24 15:32:12 +0800

    lazyman重写

---
+++ src/base/eventUtil.js
@@ -0,0 +1,25 @@
+/*
+* @description:  event库
+* @author:  Shirley(hztanxuewei@corp.netease.com)
+* @createTime:  2017-08-17, 15:00:21
+*/
+var EventUtil = {
+  addHandle: function (element, type, handler) {
+    if (element.addEventListener) {
+      element.addEventListener(type, handler, false)
+    } else if (element.attachEvent) {
+      element.attachEvent('on' + type, handler)
+    } else {
+      element['on' + type] = handler
+    }
+  },
+  removeHandle: function (element, type, handler) {
+    if (element.removeEventListener) {
+      element.removeEventListener(type, handler, false)
+    } else if (element.detachEvent) {
+      element.detachEvent('on' + type, handler)
+    } else {
+      element['on' + type] = null
+    }
+  }
+}
