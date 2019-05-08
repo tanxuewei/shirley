cde5cf454be3851df86193860a20d09bfa847e32
tanxuewei <hztanxuewei@corp.netease.com>
2019-05-08 10:33:29 +0800

    添加排序

--- demo/src/algo/sort.js
+++ demo/src/algo/sort.js
@@ -1,15 +1,15 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  for (var i = 0; i < len; i++) {
-      for (var j = 0; j < len - 1 - i; j++) {
-          if (arr[j] > arr[j+1]) {        //相邻元素两两对比
-              var temp = arr[j+1];        //元素交换
-              arr[j+1] = arr[j];
-              arr[j] = temp;
-          }
+function bubbleSort (arr) {
+  for (var i = 0, len = arr.length; i < len - 1; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j + 1]
+        arr[j + 1] = arr[j]
+        arr[j] = temp
       }
+    }
   }
-  return arr;
+
+  return arr
 }
 var arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
 console.log(bubbleSort(arr));
